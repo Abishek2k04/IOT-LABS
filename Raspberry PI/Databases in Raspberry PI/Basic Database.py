131616c45c3b7c299d0a5416557dc481a2885fc8
Abishek2k04 <www.abishekmannai2004@gmail.com>
2025-05-09 21:54:45 +0530

    Update Basic Database.py

--- Raspberry PI/Databases in Raspberry PI/Basic Database.py
+++ Raspberry PI/Databases in Raspberry PI/Basic Database.py
@@ -2,24 +2,18 @@ import sqlite3
 import datetime
 conn=sqlite3.connect("Temperature.db")
 cursor=conn.cursor()
-#cursor.execute('''CREATE TABLE TempData(Temperature FLOAT,Time FLOAT,Status VARCHAR(10))''')
-#cursor.execute("DROP TABLE IF EXISTS TempData")
-#currecntdatetime=datetime.datetime.now()
-#cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(96.2,1.26,'Normal')")
-#cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(95.4,1.30,'Normal')")
-#cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(100.2,1.34,'Abnormal')")
-#cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(101.2,1.40,'Abnormal')")
-#cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(92.2,1.16,'Normal')")
-#cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(99.4,1.20,'Normal')")
-#cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(110.2,1.44,'Abnormal')")
-#cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(125.2,1.45,'Abnormal')")
-#cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(100.2,1.50,'Abnormal')")
+cursor.execute('''CREATE TABLE TempData(Temperature FLOAT,Time FLOAT,Status VARCHAR(10))''')
+
+# cursor.execute("DROP TABLE IF EXISTS TempData")
+
+currecntdatetime=datetime.datetime.now()
+
+cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(96.2,1.26,'Normal')")
+cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(95.4,1.30,'Normal')")
+cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(100.2,1.34,'Abnormal')")
+cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(101.2,1.40,'Abnormal')")
+cursor.execute("INSERT INTO TempData(Temperature,Time,Status) VALUES(92.2,1.16,'Normal')")
+
 var = cursor.execute("SELECT Temperature FROM TempData")
 conn.commit()
-
-print(var.description[0][0])
-for i in var.fetchall():
-    print(i[0])
-#print("Table is Created")
-#print("Table is Inserted")
 conn.close()
